3af8f171bb92c4baff0520f25ff2a30fe63e3e7b
BVRXNDVRK <70912089+BVRXNDVRK@users.noreply.github.com>
2021-08-05 12:24:07 +0300

    Анимация главной страницы для мобильных телефонов

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// alert ('ddfwsd');
+alert ('\t\t\t   Venom - промосайт к фильму \n\nВерстал по видеоурокам (ДОБАВИЛ ОТСЕБЯТИНУ В ВИДЕ АНИМАЦИИ ПРИ ЗАГРУЗКЕ ГЛАВНОЙ СТРАНИЦЫ)\n\n \t\t\t\t  Технологии в проэкте: \n- Html\n- Css\n- JavaScript\n- Normalize.css\n- FansyBox');
 
+// МЕНЮ
 const menu = document.querySelector('.nav-menu');
 const menuBtn = document.querySelector('.menu-button');
 const closeMenuBtn = document.querySelector('.menu-button-close');
@@ -12,4 +13,23 @@ menuBtn.addEventListener("click", () => {
 closeMenuBtn.addEventListener("click", () => {
   menu.classList.toggle("is-open");
   closeMenuBtn.classList.toggle('is-open');
+});
+
+// АНИМАЦИЯ ПРИ ЗАГРУЗКЕ СТРАНИЦЫ
+const venom = document.querySelector('.venom');
+const filmDate = document.querySelector('.film-date');
+const filmLogo = document.querySelector('.film-logo');
+const logoImg = document.querySelector('.logo-img');
+const filmDescription = document.querySelector('.film-description');
+const buttonGroup = document.querySelector('.button-group');
+
+
+window.addEventListener("load", () => {
+  venom.classList.toggle("load");
+  filmDate.classList.toggle("load");
+  filmLogo.classList.toggle("load");
+  logoImg.classList.toggle("load");
+  menuBtn.classList.toggle("load");
+  filmDescription.classList.toggle("load");
+  buttonGroup.classList.toggle("load");
 });
